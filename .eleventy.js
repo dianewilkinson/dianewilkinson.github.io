fe04121620709356f976a01158dd7dc29df4e7aa
Diane Wilkinson <dianewilkinson510@gmail.com>
2026-02-16 20:13:30 -0800

    migrate: replace legacy static site with eleventy structure

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,14 @@ module.exports = function (eleventyConfig) {
 
   // Copy static assets directly
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("site/tools/**/demo-embed/**");
+
+  // Ignore internal documentation
+  eleventyConfig.ignores.add("internal/**");
 
   return {
     dir: {
-      input: ".",
+      input: "site",
       output: "_site"
     }
   };
