f704585ee2e7cd4da694fb376baf06c7f33adaea
Diane Wilkinson <dianewilkinson510@gmail.com>
2026-02-27 09:55:32 -0800

    calc edits

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,7 @@ module.exports = function (eleventyConfig) {
 
   // Copy static assets directly
   eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("site/tools/**/demo-embed/**");
-
-  // Ignore internal documentation
+// Ignore internal documentation
   eleventyConfig.ignores.add("internal/**");
 
   return {
